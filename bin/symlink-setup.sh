24e70be5c201b9cea7eba675334f3b76a87a945f
William Belle <william.belle@gmail.com>
2020-02-26 20:28:29 +0100

    Add os detection in symlink-setup.sh

--- bin/symlink-setup.sh
+++ bin/symlink-setup.sh
@@ -36,6 +36,24 @@ print_result() {
     && exit
 }
 
+get_os() {
+  local os=""
+  local kernelName=""
+
+  kernelName="$(uname -s)"
+
+  if [ "$kernelName" == "Darwin" ]; then
+    os="macos"
+  elif [ "$kernelName" == "Linux" ] && \
+       [ -e "/etc/os-release" ]; then
+         os="$(. /etc/os-release; printf "%s" "$ID")"
+  else
+    os="$kernelName"
+  fi
+
+  printf "%s" "$os"
+}
+
 ask_for_confirmation() {
   print_question "$1 (y/n) "
   read -n 1
